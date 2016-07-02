8f5b2f29c6a21e58afaab654a41766d623181650
NoaneDan <Noane.Dan@outlook.com>
2016-07-02 12:18:38 -0200

    eight exercise of the workshop

---
+++ ex8_httpcollectBl.js
@@ -0,0 +1,17 @@
+
+const http = require('http')
+const bl = require('bl')
+var url = process.argv[2]
+
+var result = ''
+http.get(url, function(response) {
+
+	response.pipe(bl(function (err, data) {
+		if (err) {
+			return console.log(err)
+		}
+
+		console.log(data.length)
+		console.log(data.toString())
+	}))
+})
