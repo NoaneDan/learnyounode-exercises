251cf14254a6b305d4d08ced163a13e4503247a3
NoaneDan <Noane.Dan@outlook.com>
2016-07-03 15:51:09 -0200

    eleventh exercise of the workshop

---
+++ ex11_httpfileserver.js
@@ -0,0 +1,19 @@
+
+const http = require('http'),
+      fs = require('fs');
+var port = process.argv[2],
+    file = process.argv[3];
+
+// HTTP server
+// http.createServer takes a callback which is called once for each 
+// connection. 
+// the callback takes a request and a response as parameters.
+// request: fetch properties (header, query-string)
+// response: sending data to the client (header and body)
+
+var server = http.createServer(function (req, res) {
+
+	res.writeHead(200, { 'content-type' : 'text/plain' })
+	fs.createReadStream(file).pipe(res)
+})
+server.listen(port)
