3f369fac862fcd4ec0d9a8ded09bc1423f49c742
NoaneDan <Noane.Dan@outlook.com>
2016-07-04 17:15:12 +0300

    twelfth execise of the workshop

---
+++ ex12_httpuppercaser.js
@@ -0,0 +1,18 @@
+
+const http = require('http'),
+      map = require('through2-map');
+var port = process.argv[2]
+
+var server = http.createServer(function (req, res) {
+
+	if (req.method == 'POST') {
+		req.pipe(map(function (chunk) {
+			
+			return chunk.toString().toUpperCase()
+		})).pipe(res)
+	}
+	else {
+		return res.end('')
+	}
+})
+server.listen(port)
